51eed189ebd1d32170336da371b101273720ae29
Aditya Gupta <adityagupta46772@gmail.com>
2026-09-19 23:51:51 +0530

    update(packages/schema): index.ts added password match

--- packages/schema/src/index.ts
+++ packages/schema/src/index.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 
 export const loginSchema = z.object({
     email: z.email("Please enter a valid email address"),
-    password: z.string().min(8, "Password must be at least 8 characters long")
+    password: z.string()
 });
 
 export const signUpSchema = z.object({
@@ -11,6 +11,9 @@ export const signUpSchema = z.object({
     email: z.email("Please enter a valid email address"),
     password: z.string().min(8, "Password must be at least 8 characters long"),
     confirmPassword: z.string().min(8, "Confirm Password must be at least 8 characters long")
+}).refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
 })
 
 export type LoginInput = z.infer<typeof loginSchema>;
