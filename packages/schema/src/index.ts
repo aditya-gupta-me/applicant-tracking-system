6f71921d3d538353c617f209ba13984697d50b6e
Aditya Gupta <adityagupta46772@gmail.com>
2026-09-19 23:28:24 +0530

    add(packages/schema): added zod validation for login & signup

---
+++ packages/schema/src/index.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+
+export const loginSchema = z.object({
+    email: z.email("Please enter a valid email address"),
+    password: z.string().min(8, "Password must be at least 8 characters long")
+});
+
+export const signUpSchema = z.object({
+    fullName: z.string().trim().min(1, "Name is required"),
+    email: z.email("Please enter a valid email address"),
+    password: z.string().min(8, "Password must be at least 8 characters long"),
+    confirmPassword: z.string().min(8, "Confirm Password must be at least 8 characters long")
+})
+
+export type LoginInput = z.infer<typeof loginSchema>;
+export type SignUpInput = z.infer<typeof signUpSchema>;
