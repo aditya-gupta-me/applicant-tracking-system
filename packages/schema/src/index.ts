463fcdf89e73c884ccf1136d3e5349e96fbf2c87
Aditya Gupta <adityagupta46772@gmail.com>
2026-09-21 23:45:18 +0530

    update(packages/schema): added create org zod schema

--- packages/schema/src/index.ts
+++ packages/schema/src/index.ts
@@ -15,9 +15,21 @@ export const signUpSchema = z.object({
     password: z.string().min(8, "Password must be at least 8 characters long"),
     confirmPassword: z.string().min(8, "Confirm Password must be at least 8 characters long")
 }).refine((data) => data.password === data.confirmPassword, {
-    message: "Passwords do not match",
+    error: "Passwords do not match",
     path: ["confirmPassword"],
+})
+
+export const createOrganizationSchema = z.object({
+    name: z.string().trim().min(1, "Organization name is required"),
+    website: z.url("Invalid URL layout"),
+    email: z.email("Please enter a valid email address"),
+    image: z.string().optional(),
+    establishedDate: z.coerce.date()
+    .refine((date) => date <= new Date(), {
+        error: "Established date cannot be in the future"
+    })
 })
 
 export type LoginInput = z.infer<typeof loginSchema>;
 export type SignUpInput = z.infer<typeof signUpSchema>;
+export type OrganizationInput = z.infer<typeof createOrganizationSchema>;
